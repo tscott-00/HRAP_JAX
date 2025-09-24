ea438a62b5a079d7983c2bc08fb88db03ea5e8ae
Thomas Scott <81178161+tscott-00@users.noreply.github.com>
2025-09-24 15:17:57 -0500

    webview js dist w/ npm build, soon to be removed

--- HRAP - Python/hrap/gui/frontend/src/App.vue
+++ HRAP - Python/hrap/gui/frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Tank from './components/Tank.vue'
+
 </script>
 
 <template>
@@ -12,36 +14,65 @@ import TheWelcome from './components/TheWelcome.vue'
     </div>
   </header>
 
+  <!-- <main> -->
+    <!-- <TheWelcome /> -->
+  <!-- </main> -->
   <main>
-    <TheWelcome />
+    <Tank/>
   </main>
+  
+  <button class="btn btn-primary">Button</button>
+  <input type="text" placeholder="Type here" class="input" />
+  
+  <input type="range" min="0" max="100" value="40" class="range" />
+  
+  <label class="input">
+    <span class="label">Fill level</span>
+    <input type="range" min="0" max="100" value="70" class="range" />
+    <input type="text" placeholder="70%" />
+  </label>
+  
+  <label class="select">
+    <span class="label">Oxidizer</span>
+    <select>
+      <option>Nitrous Oxide</option>
+      <option>Oxygen</option>
+    </select>
+  </label>
+  
+  <label class="input">
+    <input type="text" placeholder="domain name" />
+    <span class="label">.com</span>
+  </label>
+  
+  <div class="skeleton h-32 w-32"></div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<!-- <style scoped> -->
+<!-- header { -->
+  <!-- line-height: 1.5; -->
+<!-- } -->
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<!-- .logo { -->
+  <!-- display: block; -->
+  <!-- margin: 0 auto 2rem; -->
+<!-- } -->
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<!-- @media (min-width: 1024px) { -->
+  <!-- header { -->
+    <!-- display: flex; -->
+    <!-- place-items: center; -->
+    <!-- padding-right: calc(var(--section-gap) / 2); -->
+  <!-- } -->
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  <!-- .logo { -->
+    <!-- margin: 0 2rem 0 0; -->
+  <!-- } -->
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  <!-- header .wrapper { -->
+    <!-- display: flex; -->
+    <!-- place-items: flex-start; -->
+    <!-- flex-wrap: wrap; -->
+  <!-- } -->
+<!-- } -->
+<!-- </style> -->
