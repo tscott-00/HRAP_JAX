011c6f7ce016208452cdbdac7ba8b2626784a9c3
Thomas A. Scott <81178161+tscott-00@users.noreply.github.com>
2026-02-10 18:58:50 -0600

    note

--- HRAP - Python/hrap/gui/main.py
+++ HRAP - Python/hrap/gui/main.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# Doesn't work, needs to be python subfolder...
 # pip install --force-reinstall git+https://github.com/tscott-00/HRAP_JAX@webview
 
 import hrap
